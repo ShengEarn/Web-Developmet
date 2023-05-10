27466960150bd9fdd8a654618c47562db3a730b8
ShengEarn <88524504+ShengEarn@users.noreply.github.com>
2023-05-11 00:57:05 +0800

    change some design

--- product.js
+++ product.js
@@ -1,13 +1,23 @@
+let quantity = document.getElementById("myNumber");
+
 function incrementNumber() {
-  var inputField = document.getElementById("myNumber");
-  if (parseInt(inputField.value) < parseInt(inputField.max)) {
-    inputField.value = parseInt(inputField.value) + 1;
-  }
+  quantity.value++;
 }
 
 function decrementNumber() {
-  var inputField = document.getElementById("myNumber");
-  if (parseInt(inputField.value) > parseInt(inputField.min)) {
-    inputField.value = parseInt(inputField.value) - 1;
+  if (quantity.value > 1) {
+    quantity.value--;
   }
+}
+
+function addToCart() {
+  let item = {
+    name: "MAXX FITNESS FID BENCH W/ARM PAD & LEG DEVELOPER",
+    price: 495.0,
+    quantity: parseInt(quantity.value),
+  };
+  let cart = JSON.parse(localStorage.getItem("cart")) || [];
+  cart.push(item);
+  localStorage.setItem("cart", JSON.stringify(cart));
+  alert("Item added to cart!");
 }
